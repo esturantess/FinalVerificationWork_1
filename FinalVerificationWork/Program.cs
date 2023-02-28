1fa96793c76a9122d06ae674f47a7b534edabda3
Polina <ms.maylin@inbox.ru>
2023-02-28 17:01:20 +0300

    In FinalVerificationWork: method LeaveElementsOfSpecifiedLengthInArray added in program.cs

--- FinalVerificationWork/Program.cs
+++ FinalVerificationWork/Program.cs
@@ -11,3 +11,20 @@ int NumberElementsOfSpecifiedLength(string[] array, int size)
     }
     return count;
 }
+
+string[] LeaveElementsOfSpecifiedLengthInArray(string[] array, int size)
+{
+    string[] newArray = new string[NumberElementsOfSpecifiedLength(array, size)];
+
+    int j = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= size)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
