788d19224239c82f7e9e914491b2a3947719f18c
Polina <ms.maylin@inbox.ru>
2023-02-28 17:00:15 +0300

    In FinalVerificationWork: method NumberElementsOfSpecifiedLength added in program.cs

--- FinalVerificationWork/Program.cs
+++ FinalVerificationWork/Program.cs
@@ -1,3 +1,13 @@
 ﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых
 // меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте
 // выполнения алгоритма.
+
+int NumberElementsOfSpecifiedLength(string[] array, int size)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= size) count++;
+    }
+    return count;
+}
